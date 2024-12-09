12b9e37a0878399f35bb31110d566a14e95505c1
Divya Prakash <prakashpreveen@gmail.com>
2024-12-09 09:50:26 +0530

    added subtask and ADD TASK button

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./NewTask.css";
 import SubTask from "./SubTask";
 
@@ -9,11 +9,31 @@ const NewTask = ({
   taskDescription,
   setTaskDescription,
 }) => {
-  const [subTask, setSubTask] = useState(false);
+  const MyRef = useRef();
+
   const [newSubTasks, setNewSubTasks] = useState([]);
+  const [isFormVaild, setIsFormValid] = useState(true);
+
+  useEffect(() => {
+    MyRef.current.focus();
+  }, []);
 
   const SubTaskHandler = () => {
     setNewSubTasks((prevSubTasks) => [...prevSubTasks, ""]); // Add a new empty subtask
+  };
+
+  const AddTaskHandler = (e) => {
+    e.preventDefault();
+    if (!taskName.trim() || !taskDescription.trim()) {
+      setIsFormValid(false);
+      return;
+    } else {
+      setTaskName("");
+      setTaskDescription("");
+      setNewSubTasks([]);
+      setNewTask(false);
+      setIsFormValid(true);
+    }
   };
 
   return (
@@ -21,18 +41,25 @@ const NewTask = ({
       <div id="overlay" onClick={() => setNewTask(false)} />
       <div id="newtask-form">
         <input
+          ref={MyRef}
           className="project-name"
           type="text"
           placeholder="Project Name"
           value={taskName}
-          onChange={(e) => setTaskName(e.target.value)}
+          onChange={(e) => setTaskName(e.target.value.toUpperCase())}
           required
+          style={{
+            borderColor: isFormVaild || taskName.trim() ? "grey" : "red",
+          }}
         />
         <textarea
           className="project-desc"
           placeholder="Project Description"
           value={taskDescription}
           onChange={(e) => setTaskDescription(e.target.value)}
+          style={{
+            borderColor: isFormVaild || taskDescription.trim() ? "grey" : "red",
+          }}
         />
         <>
           <button className="subtaskBtn" onClick={SubTaskHandler}>
@@ -40,6 +67,9 @@ const NewTask = ({
           </button>
         </>
         <SubTask newSubTasks={newSubTasks} setNewSubTasks={setNewSubTasks} />
+        <button onClick={AddTaskHandler} className="addtaskBtn">
+          ADD TASK
+        </button>
       </div>
     </>
   );
